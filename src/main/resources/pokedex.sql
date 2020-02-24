9b9578b29c08b092a690f5c03feb9a89da3a0f3a
inigo hernandez <ihernandezzarraga@gmail.com>
2020-02-24 13:58:27 +0100

    Añadido el script de la base de datos actualizado

--- src/main/resources/pokedex.sql
+++ src/main/resources/pokedex.sql
@@ -44,9 +44,9 @@ CREATE TABLE IF NOT EXISTS `pokemon` (
   `imagen` varchar(800) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nombre` (`nombre`)
-) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla pokedex.pokemon: ~11 rows (aproximadamente)
+-- Volcando datos para la tabla pokedex.pokemon: ~16 rows (aproximadamente)
 DELETE FROM `pokemon`;
 /*!40000 ALTER TABLE `pokemon` DISABLE KEYS */;
 INSERT INTO `pokemon` (`id`, `nombre`, `imagen`) VALUES
@@ -57,10 +57,11 @@ INSERT INTO `pokemon` (`id`, `nombre`, `imagen`) VALUES
 	(8, 'pidgeot', 'https://cdn.memegenerator.es/imagenes/memes/full/4/98/4986840.jpg'),
 	(9, 'charizard', 'https://pm1.narvii.com/6315/840550f925ad1444e2d38d1cd05638f9da3f3c23_hq.jpg'),
 	(10, 'squirtle', 'https://pm1.narvii.com/6770/cabbd48cdabe20fa2cfcd8cdd141baac7ad1e891v2_hq.jpg'),
-	(17, 'charmeleon', 'https://images.wikidexcdn.net/mwuploads/wikidex/f/fb/latest/20190805232138/Charmeleon.png'),
-	(31, 'diglett', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/050.png'),
+	(17, 'charmeleon', 'https://www.geek.com/wp-content/uploads/2019/04/pantherchameleon1-625x352.jpg'),
 	(53, 'riolu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/447.png'),
-	(54, 'Wartortle', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/080.png');
+	(58, 'Duralodon', 'https://cdn.bulbagarden.net/upload/3/38/884Duraludon.png'),
+	(76, 'Metagross', 'https://www.alfabetajuega.com/abj_public_files/multimedia/imagenes/201410/83556.alfabetajuega-recopilatiorio-megaevoluciones-pokemon-36-071014.jpg'),
+	(78, 'pichu', 'https://pm1.narvii.com/6589/96e02d76c7eeebbbd9b64aa01bbeaf7504158bf6_hq.jpg');
 /*!40000 ALTER TABLE `pokemon` ENABLE KEYS */;
 
 -- Volcando estructura para tabla pokedex.pokemon_has_habilidades
@@ -74,15 +75,12 @@ CREATE TABLE IF NOT EXISTS `pokemon_has_habilidades` (
   KEY `fk_pokemon` (`id_pokemon`),
   CONSTRAINT `fk_habilidad` FOREIGN KEY (`id_habilidad`) REFERENCES `habilidad` (`id`),
   CONSTRAINT `fk_pokemon` FOREIGN KEY (`id_pokemon`) REFERENCES `pokemon` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla pokedex.pokemon_has_habilidades: ~14 rows (aproximadamente)
+-- Volcando datos para la tabla pokedex.pokemon_has_habilidades: ~25 rows (aproximadamente)
 DELETE FROM `pokemon_has_habilidades`;
 /*!40000 ALTER TABLE `pokemon_has_habilidades` DISABLE KEYS */;
 INSERT INTO `pokemon_has_habilidades` (`id`, `id_pokemon`, `id_habilidad`) VALUES
-	(1, 1, 1),
-	(2, 1, 2),
-	(3, 1, 3),
 	(4, 2, 4),
 	(5, 2, 5),
 	(8, 3, 1),
@@ -90,10 +88,17 @@ INSERT INTO `pokemon_has_habilidades` (`id`, `id_pokemon`, `id_habilidad`) VALUE
 	(6, 3, 6),
 	(10, 4, 1),
 	(9, 4, 7),
-	(26, 17, 2),
-	(11, 17, 6),
-	(29, 54, 3),
-	(28, 54, 4);
+	(84, 17, 1),
+	(82, 17, 2),
+	(85, 17, 4),
+	(83, 17, 6),
+	(34, 58, 3),
+	(33, 58, 4),
+	(72, 76, 4),
+	(80, 78, 1),
+	(81, 78, 2),
+	(78, 78, 4),
+	(79, 78, 5);
 /*!40000 ALTER TABLE `pokemon_has_habilidades` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
